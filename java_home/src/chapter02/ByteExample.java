4b4cacee28167994d5fa36895146efd3523e4aec
Dar wook <m4786@naver.com>
2024-04-26 09:54:05 +0900

    24-04-26 리터럴

---
+++ java_home/src/chapter02/ByteExample.java
@@ -0,0 +1,13 @@
+package chapter02;
+
+public class ByteExample {
+
+	public static void main(String[] args) {
+		int var1= -129;
+//		byte는 2비트로 -128~127까지의 범위이기 때문이다.
+//		byte var2= -129;
+	
+		System.out.println(var1);
+	}
+
+}
